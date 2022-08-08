09f64a07b80c7c7c71e21175b81363e71bc1598b
Jan Wazowski <yanhatskevich@mail.com>
2022-08-08 19:33:26 +0200

    Styled the page in css

--- script.js
+++ script.js
@@ -1,54 +1,31 @@
-const btn = document.querySelector('.btn')
-const yourScore = document.querySelector('.your-score')
+const btn = document.querySelector('.btn');
+const yourScore = document.querySelector('.your-score');
+const inputs = document.querySelectorAll('.correct');
 
-btn.addEventListener('click', result)
+const inputsArr = [...inputs];
 
-function result(argument) {
-	let score = 0
-	if (document.getElementById('correct1').checked) {
-		score++
-	}
-	if (document.getElementById('correct2').checked) {
-		score++
-	}
-	if (document.getElementById('correct3').checked) {
-		score++
-	}
-	if (document.getElementById('correct4').checked) {
-		score++
-	}
-	if (document.getElementById('correct5').checked) {
-		score++
-	}
-	if (document.getElementById('correct6').checked) {
-		score++
-	}
-	if (document.getElementById('correct7').checked) {
-		score++
-	}
-	if (document.getElementById('correct8').checked) {
-		score++
-	}
-	if (document.getElementById('correct9').checked) {
-		score++
-	}
-	if (document.getElementById('correct10').checked) {
-		score++
-	}
+btn.addEventListener('click', results);
 
-	if (score >= 5 && score == 10) {
-		yourScore.append(
-			`Your score is ${score}. It looks like you have a severe drinking problem. You might need to seek help of a specialist.`
-		)
-	} else if (score < 5 && score > 2) {
-		yourScore.append(
-			`Your score is ${score}. Your drinking problem is probably not that bad yet and it can get worse if you do not work on it.`
-		)
+function results(e) {
+	e.preventDefault();
+	let score = 0;
+	inputsArr.map(el => {
+		if (el.checked) {
+			score++;
+		} else {
+			score + 0;
+		}
+	});
+
+	if (score >= 5 && score <= 10) {
+		yourScore.innerHTML = `Your score is ${score}. It looks like you have a severe drinking problem. You might need to seek help of a specialist.`;
+	} else if (score < 5 && score >= 2) {
+		yourScore.innerHTML = `Your score is ${score}. Your drinking problem is probably not that bad yet and it can get worse if you do not work on it.`;
 	} else {
-		yourScore.append(
-			`Apparently you do not have any drinking issues. Please keep going like this and you will be fine.`
-		)
+		yourScore.innerHTML = `Apparently you do not have any drinking issues. Please keep going like this and you will be fine.`;
 	}
-
-	// The idea is to display the results in html in Results section. For each 'yes' the user gets 1 point.when there is no point
+	score = '';
+	document.myForm.reset();
 }
+
+// The idea is to display the results in html in Results section. For each 'yes' the user gets 1 point.when there is no point
